b52bf43b39006801e9041f95d241f8e1be77506f
Johann-S <johann.servoire@gmail.com>
2019-09-05 10:14:30 +0200

    add validate tasks to check if everything is fine

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,9 +1,39 @@
 'use strict'
 
 const gulp = require('gulp')
+const fs = require('fs')
+const cheerio = require('cheerio')
 const gulpImgLqip = require('..')
 
-gulp.task('default', () => {
-  return gulp.src('*.html')
-    .pipe(gulpImgLqip(__dirname))
-})
+const fileList = [
+  'index.html',
+  'test.html'
+]
+
+const lqip = () => gulp.src('*.html').pipe(gulpImgLqip(__dirname))
+const validate = () => {
+  const expectedErrors = 2
+  let errors = 0
+
+  fileList.forEach(filePath => {
+    const fileData = fs.readFileSync(filePath, { encoding: 'utf8' })
+
+    const $ = cheerio.load(fileData)
+
+    $('img').each((index, element) => {
+      if (!$(element).attr('data-src')) {
+        errors++
+      }
+    })
+  })
+
+  if (errors !== expectedErrors) {
+    Promise.reject(new Error(`Some images doesn't have a data-src attribute (expected ${expectedErrors} got ${errors})`))
+
+    return
+  }
+
+  return Promise.resolve()
+}
+
+gulp.task('default', gulp.series(lqip, validate))
